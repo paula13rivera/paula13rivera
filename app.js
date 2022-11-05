3b45b48042c2a120f356bb3aa160468d139c402b
paula13rivera <117528431+paula13rivera@users.noreply.github.com>
2022-11-05 10:41:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('inicio de programa');
+
+setTimeout(()=>{
+     HTMLFormControlsCollection.log('Primer TimeOut');
+},3000);
+
+setTimeout(()=>{
+    console.log('segundo timeout')
+
+},0;)
+
+setTimeout(()=>{
+    console.log('tercero timeout')
+
+},0;)
+
+console.log('Fin del progrema');
